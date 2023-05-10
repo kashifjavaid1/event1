256500a018ca4bb37b272a17341b043ad67e5dab
kashifjavaid1 <gmdcf797@gmail.com>
2023-05-10 21:01:37 +0500

    script.js
    onmouseover and onmouse out

---
+++ script.js
@@ -0,0 +1,8 @@
+function even(params) {
+    document.getElementById("username").className="bg1";
+    document.getElementById("username").style="bg1";
+}
+function out(params) {
+    document.getElementById("username").className="bg2";
+    document,getElementById("username").style="bg2";
+}
